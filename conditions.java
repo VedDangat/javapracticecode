65d2962d22b4caed88ec3ad9b708437ae93d09bc
Ved Dangat <veddangat123@gmail.com>
2026-07-18 14:18:08 +0530

    Added new Java files

---
+++ conditions.java
@@ -0,0 +1,142 @@
+/*
+import java.util.*;
+public class conditions{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter the age of student:");
+        int age=sc.nextInt();
+
+        if(age>18){
+            System.out.println("eligible to vote and register for driving licence");
+        }
+        else{
+            System.out.println("not eligble to vote and register for driving licence ");
+        }
+        sc.close();
+    }
+}
+
+
+import java.util.*;
+public class conditions{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter the number");
+        int num=sc.nextInt();
+
+        if(num%2==0){
+            System.out.println("even number");
+
+        }
+        else{
+            System.out.println("odd number");
+        }
+        sc.close();
+    }
+}
+
+
+import java.util.*;
+public class conditions{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter the value of a:");
+        int a=sc.nextInt();
+
+        System.out.print("enter the value of b:");
+        int b=sc.nextInt();
+
+        if(a==b){
+            System.out.println("a and b are equal");
+        }
+        else if(a>b){
+            System.out.println("a is greate theb b");
+        }
+        else{
+            System.out.println("a is less than b");
+        }
+        sc.close();
+    }
+}
+
+
+import java.util.*;
+public class conditions{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the button value eiter 1,2,3 and 4:");
+        int button=sc.nextInt();
+
+        switch(button){
+            case 1:
+                System.out.print("namaste");
+                break;
+
+            case 2:
+                System.out.print("hello");
+                break;
+
+            case 3:
+                System.out.print("hola");
+                break;
+
+            case 4:
+                System.out.print("bonjur");
+                break;
+
+            default:
+                System.out.println("invalid number entered");
+            
+            }
+            sc.close();
+    }
+}
+*/
+
+import java.util.*;
+public class conditions{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter the value of a-");
+        int a=sc.nextInt();
+
+        System.out.print("enter value of b-");
+        int b=sc.nextInt();
+
+        System.out.print("etner the number 1,2,3,4-");
+        int numb=sc.nextInt();
+
+        switch(numb){
+
+            case 1:
+                int sum=a+b;
+                System.out.print(sum);
+                break;
+
+            case 2:
+                int sub=a-b;
+                System.out.print(sub);
+                break;
+            
+            case 3:
+                int mul=a*b;
+                System.out.print(mul);
+                break;
+
+            case 4:
+                int div=a/b;
+                System.out.print(div);
+                break;
+
+            default:
+                System.out.print("invalid number entered no operqation"); 
+        }
+        sc.close();
+    }
+}
