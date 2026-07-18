2610c970362e4ef976d0c1de043163d690c2aedb
Ved Dangat <veddangat123@gmail.com>
2026-07-18 13:16:37 +0530

    Initial Commit

---
+++ javaoutput.java
@@ -0,0 +1,175 @@
+/*public class javaoutput {
+    public static void main(String[]args)
+    {
+        System.out.println("hello everyone");
+        System.out.print("this is my first java program\n");
+        System.out.print("Learning java fom appna college");
+    }
+}
+
+public class  javaoutput{
+    public static void main(String[]args)
+    {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.print("****");
+    }
+}
+
+public class javaoutput{
+    public static void main(String[]args)
+    {
+      String name="veddangat";
+      int a=20;
+      int b=30;
+      int c;
+
+      c=a+b;
+
+      System.out.println(c);
+      System.out.print(name);
+    }
+}
+
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        int a=50;
+        int b=60;
+        int sum;
+        int sub;
+        int mul;
+        int div;
+
+        sum=a+b;
+        System.out.println(sum);
+
+        sub=a-b;
+        System.out.println(sub);
+
+        mul=a*b;
+        System.out.println(mul);
+
+        div=a/b;
+        System.out.println(div);
+    }
+}
+
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        String name="veddangat";
+        System.out.print(name);
+    }
+}
+
+
+import java.util.*;
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.print(name);
+    }
+
+}
+
+import java.util.*;
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int sum=a+b;
+
+        System.out.println(sum);
+    }
+}
+
+
+import java.util.*;
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter value of a:");
+        int a=sc.nextInt();
+
+        System.out.print("enter value of b:");
+        int b=sc.nextInt();
+
+        System.out.println();
+
+        int sum=a+b;
+        System.out.println(sum);
+
+        int sub=a-b;
+        System.out.println(sub);
+
+        int mul=a*b;
+        System.out.println(mul);
+
+        int div=a/b;
+        System.out.println(div);
+
+        int mod=a%b;
+        System.out.println(mod);
+    }
+}
+
+import java.util.Scanner;
+
+public class javaoutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the circumference: ");
+        double circum = sc.nextDouble();
+
+        double pi = 3.14159;
+        double radius = circum / (2 * pi);
+
+        System.out.print(radius);
+
+        sc.close();
+    }
+}
+
+
+
+import java.util.*;
+public class javaoutput{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the value of a");
+        int a=sc.nextInt();
+
+        System.out.println("enter value of b");
+        int b=sc.nextInt();
+
+        System.out.print("\n");
+
+        int  sum=a+b;
+        System.out.println(sum);
+
+        int sub=a-b;
+        System.out.println(sub);
+
+        int mul=a*b;
+        System.out.println(mul);
+
+        int div=a/b;
+        System.out.println(div);
+        
+        sc.close();
+    }
+}
+
+*/
